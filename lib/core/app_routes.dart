7d106339560cd12ba3f51c701b31a08ecdf4fa0c
deepeshkalurs <deepeshkalurs@gmail.com>
2024-06-14 17:57:42 +0000

    implementation of go router in the application

---
+++ lib/core/app_routes.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../presentation/about_us_screen.dart';
+import '../presentation/authentication_screen.dart';
+import '/core/app_url.dart';
+import '../presentation/splash_screen.dart';
+import '../presentation/home_screen.dart';
+
+final rootNavigatorKey = GlobalKey<NavigatorState>();
+
+class AppRoutes {
+  static final router = GoRouter(
+    initialLocation: '/',
+    navigatorKey: rootNavigatorKey,
+    routes: [
+      GoRoute(
+        path: '/',
+        name: AppUrl.splashScreen,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: '/homescreen',
+        name: AppUrl.homeScreen,
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/signin',
+        name: AppUrl.signInScreen,
+        builder: (context, state) => const SignInScreen(),
+      ),
+      GoRoute(
+        path: '/aboutus',
+        name: AppUrl.aboutUsScreen,
+        builder: (context, state) => const AboutUsScreen(),
+      ),
+    ],
+  );
+}
---
+++ lib/core/app_url.dart
@@ -0,0 +1,7 @@
+class AppUrl {
+  static const String splashScreen = "splashScreen";
+  static const String homeScreen = "homeScreen";
+  static const String signInScreen = "signInScreen";
+  static const String aboutUsScreen = "aboutUsScreen";
+
+}
